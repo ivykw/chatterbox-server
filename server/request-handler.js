b202dbcb3fe0609499f5523452702b3f7406d800
ivykw <ivyw219@gmail.com>
2022-03-11 19:28:33 -0600

    Post response

--- server/request-handler.js
+++ server/request-handler.js
@@ -20,6 +20,41 @@ this file and include it in basic-server.js so that it actually works.
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
+
+
+
+// Request and Response come from node's http module.
+//
+// They include information about both the incoming request, such as
+// headers and URL, and about the outgoing response, such as its status
+// and content.
+//
+// Documentation for both request and response can be found in the HTTP section at
+// http://nodejs.org/documentation/api/
+
+// Do some basic logging.
+//
+// Adding more logging to your server can be an easy way to get passive
+// debugging help, but you should always be careful about leaving stray
+// console.logs in your code.
+// The outgoing status.
+// See the note below about CORS headers.
+// Tell the client we are sending them plain text.
+//
+// You will need to change this if you are sending something
+// other than plain text, like JSON or HTML.
+
+// .writeHead() writes to the request line and headers of the response,
+// which includes the status and all headers.
+
+// Make sure to always call response.end() - Node may not send
+// anything back to the client until you do. The string you pass to
+// response.end() will be the body of the response - i.e. what shows
+// up in the browser.
+//
+// Calling .end "flushes" the response's internal buffer, forcing
+// node to actually send all the data over to the client.
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -27,65 +62,6 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  var array = [];
-  // The outgoing status.
-  var statusCode = 200;
-  if (request.url !== '/classes/messages') {
-    statusCode = 404;
-  }
-  if (request.method === 'POST') {
-    statusCode = 201;
-    let body = [];
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      body = Buffer.concat(body).toString();
-      array.push(JSON.parse(body));
-      response.end(JSON.stringify(array));
-    });
-
-  }
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end(JSON.stringify(array));
-};
-
-
-
-module.exports.handleRequest = requestHandler;
 // var array = [
 //   { username: 'Test', roomname: 'Lobby', text: 'Test test'},
 //   { username: 'Test', roomname: 'Lobby', text: 'Test test'},
@@ -98,3 +74,35 @@ module.exports.handleRequest = requestHandler;
 //   { username: 'Test', roomname: 'Lobby', text: 'Test test'},
 //   { username: 'Test', roomname: 'Lobby', text: 'Test test'},
 // ];
+
+var requestHandler = function(request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  var array = [];
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/json';
+  if (request.url !== '/classes/messages') {
+    statusCode = 404;
+  }
+  if (request.method === 'POST') {
+    statusCode = 201;
+    let body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      array.push(body);
+      array = JSON.stringify(array);
+      console.log(array);
+      response.writeHead(201, headers);
+      response.end(array);
+    });
+
+  }
+  // response.writeHead(statusCode, headers);
+  // response.end(JSON.stringify(array));
+};
+
+
+
+module.exports.handleRequest = requestHandler;
